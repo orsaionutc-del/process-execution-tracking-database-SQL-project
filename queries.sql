0b6daa8ca04a2ce067145452d65ef886cefb7875
orsaionutc-del <orsaionutc@gmail.com>
2026-09-11 20:33:37 +0300

    Update queries.sql
    new queries

--- queries.sql
+++ queries.sql
@@ -1,47 +1,3 @@
---SQL query to see all the batches that a user ran
-
-SELECT u.first_name , u.last_name , e.batch_id
-FROM EXECUTIONS as e
-JOIN
-USERS as u
-on u.user_id = e.user_id ;
-
---SQL query that inserts another user in the user table
-
-INSERT INTO USERS(first_name, last_name)
-VALUES ('Ionut','Orsa')
-
---SQL query that insers another process in the processes table
-
-INSERT INTO PROCESSES(name)
-VALUES('VAT Clearing') ;
-
---SQL query that changes the name intorduced wrong of a user
-
-UPDATE USERS
-SET first_name = 'Ionut'
-SET last_name = 'Orsa'
-WHERE id = '1'
-
---SQL query that changes the name introduced wrong of a process
-
-UPDATE PROCESSES
-SET name = 'VAT Clearing'
-WHERE id = (
-    SELECT id
-    FROM USERS
-    WHERE name = 'VAT Clearin'
-)
-
---SQL query to eliminate a user
-
-DELETE FROM users
-WHERE user_id = (
-    SELECT user_id
-    FROM USERS
-    WHERE first_name = 'Ionut' AND last_name = 'Orsa'
-)
-
 --SQL query that returns input and output for an execution
 
 WITH exec_info AS (
@@ -60,7 +16,7 @@ JOIN INPUTS i
 JOIN OUTPUTS o
     ON e.execution_id = o.execution_id;
 
---SQL query that returns top 3 processes with the most failed executions and the number of failed executions so the dev team can improve them
+-- SQL query that returns top 3 processes with the most failed executions and the number of failed executions so the dev team can improve them
 
 WITH error_count AS (
 SELECT p.name, COUNT(p.process_id) AS error_num
@@ -79,4 +35,45 @@ SELECT name, error_num
 FROM ranking
 WHERE rank < 4
 ORDER BY rank
+
+-- Failure rate per process from the last two weeks to check what process should be removed from prod due to recent changes
+
+WITH error_count AS (
+SELECT p.name, COUNT(p.process_id) AS error_num, e.executed_at
+FROM EXECUTIONS AS e
+JOIN PROCESSES AS p
+  ON p.process_id = e.process_id
+WHERE e.status = 'FAILED' AND p.deleted = 0 AND DATEDIFF(CURRENT_DATE,e.executed_at) < 14
+GROUP BY p.process_id ),
+
+total_count AS (SELECT p.name, COUNT(p.process_id) AS total_proc, e.executed_at
+FROM EXECUTIONS AS e
+JOIN PROCESSES AS p
+  ON p.process_id = e.process_id
+WHERE p.deleted = 0 AND DATEDIFF(CURRENT_DATE,e.executed_at) < 14
+GROUP BY p.process_id )
+
+SELECT e.name, ROUND(e.error_num/t.total_proc*100,2) AS fail_percentage
+FROM error_count AS e
+JOIN total_count AS t
+ON e.name = t.name
+ORDER BY ROUND(e.error_num/t.total_proc*100,2) DESC
+
+-- Ranking of the batches started by all the users
+
+WITH b_count AS (
+SELECT 
+COALESCE(u.user_id,'Unknown') AS user_id, 
+COALESCE(CONCAT(u.first_name,u.last_name),'Unknown') AS full_name, 
+COUNT(DISTINCT(e.batch_id)) AS batch_count
+FROM EXECUTIONS AS e
+LEFT JOIN USERS AS u 
+ON e.user_id = u.user_id
+GROUP BY e.batch_id
+)
+
+SELECT user_id, full_name, batch_count, DENSE_RANK() OVER (ORDER_BY batch_count) AS rank
+FROM b_count
+ORDER BY  DENSE_RANK() OVER (ORDER_BY batch_count) DESC
+
 
